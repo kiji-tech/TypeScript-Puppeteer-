b7b3f0257ea9230a8711243bcac0594b264db91e
shinji5761 <shinji.tec.5761@gmail.com>
2021-09-07 03:41:24 +0900

    第3回 TypeScript×Puppeteerでスクレイピングを始めよう

---
+++ batch/src/main/company.ts
@@ -0,0 +1,15 @@
+export class Company {
+    code     : string = '';
+    market   : string = '';
+    name     : string = '';
+    value    : string = '';
+    dividend : string = '';
+    rate     : string = '';
+    sector   : string = '';
+    per      : string = '';
+    pbr      : string = '';
+    eps      : string = '';
+    bps      : string = '';
+    num      : string = '';
+    detail_page : string = '';
+}
--- batch/src/main/index.ts
+++ batch/src/main/index.ts
@@ -1,35 +1,25 @@
 import { Browser, Page } from "puppeteer";
+import { Company } from './company';
 
 const puppeteer = require('puppeteer');
 const target_url = "https://info.finance.yahoo.co.jp/ranking/?kd=8&mk=1&tm=d&vl=a";
 
 
-class Company {
-    code     : string = '';
-    market   : string = '';
-    name     : string = '';
-    value    : string = '';
-    dividend : string = '';
-    rate     : string = '';
-
-}
-
-
-(async () => {
+( async () => {
     // browserを立ち上げる
     let browser;
     // page(tab)を生成する
     let page;
     
     try {
-        browser = await puppeteer.launch() as Browser;
+        browser = await puppeteer.launch({'headless' : true, 'slowMo' : 0}) as Browser;
         page    = await browser.newPage() as Page;
         
         // 特定のURLへ移動する｡
         // ページ読み込みができるまで待つ
         await Promise.all([
-            page.goto(target_url),
-            page.waitForNavigation({ 'waitUntil' : 'domcontentloaded', 'timeout' : 30000 })
+            page.waitForNavigation({ 'waitUntil' : 'domcontentloaded', 'timeout' : 30000 }),
+            page.goto(target_url)
         ]);
     
         // 遷移先のページ確認
@@ -37,72 +27,129 @@ class Company {
         if( dom.indexOf("rankingTableWrapper") < 0 ) {
             throw new Error( "ページ遷移 失敗" );
         }
-        let result_list : Company[] = [];
-
+  
         while( true ) {
             // ページ内の銘柄表を取得
-            const company_list = await page.$$('div.rankingTableWrapper > table > tbody > tr');
+            const company_list : Company[] = 
+            await page.$$eval('#contents-body-bottom > div.rankdata > div.rankingTableWrapper > table > tbody > tr', ( company_selector_list => company_selector_list.map( company => {
+                let c = {} as Company;
+                console.log( c );
 
-            // 1行ずつ処理をする
-            for(let company of company_list) {
                 // コードのラベルを取得
-                let c = new Company();
-                const code_item = await company.$("td:nth-child(2) > a");
+                const code_item = company.querySelector("td:nth-child(2) > a") as HTMLLinkElement;
                 if( code_item ) {
-                    const item = await code_item.getProperty('textContent');
-                    if( item )
-                        c.code = await item.jsonValue();
+                    c.code = code_item.innerHTML;
+                    c.detail_page = code_item.href;
+                }
+
+                // 市場のラベルを取得
+                const market_item = company.querySelector("td:nth-child(3)") as HTMLElement;
+                if( market_item ) {
+                    c.market = market_item.innerHTML;
                 }
                 
-                // 市場のラベルを取得
-                const market_item = await company.$("td:nth-child(3)");
-                if( market_item ) {
-                    const item = await market_item.getProperty('textContent');
-                    if( item )
-                        c.market = await item.jsonValue();
-                }
-
                 // 名称のラベルを取得
-                const name_item = await company.$("td:nth-child(4)");
+                const name_item = company.querySelector("td:nth-child(4)") as HTMLElement;
                 if( name_item ) {
-                    const item = await name_item.getProperty('textContent');
-                    if( item )
-                    c.name = await item.jsonValue();
+                    c.name = name_item.innerHTML;
                 }
                 
                 // 取引値のラベルを取得
-                const value_item = await company.$("td:nth-child(6)");
+                const value_item = company.querySelector("td:nth-child(6)") as HTMLElement;
                 if( value_item ) {
-                    const item = await value_item.getProperty('textContent');
-                    if( item )
-                    c.value = await item.jsonValue();
+                    c.value = value_item.innerHTML;
                 }
 
                 // 1株配当のラベルを取得
-                const dividend_item = await company.$("td:nth-child(8)");
+                const dividend_item = company.querySelector("td:nth-child(8)") as HTMLElement;
                 if( dividend_item ) {
-                    const item = await dividend_item.getProperty('textContent');
-                    if( item )
-                    c.dividend = await (item).jsonValue();
+                    c.dividend = dividend_item.innerHTML;
                 }
 
                 // 配当利回りのラベルを取得
-                const rate_item = await company.$("td:nth-child(9)");
+                const rate_item = company.querySelector("td:nth-child(8)") as HTMLElement;
                 if( rate_item ) {
-                    const item = await rate_item.getProperty('textContent');
-                    if( item )
-                        c.rate = await (item).jsonValue();
+                    c.rate = rate_item.innerHTML;
                 }
-                console.log(JSON.stringify(c)); 
+                return c;
+            })));
+
+            // 1行ずつ処理をする
+            for(let c of company_list) {
+                // 詳細ページへ画面遷移
+                await Promise.all([
+                    page.waitForNavigation({'waitUntil' : 'networkidle2', 'timeout' : 30000 }),
+                    page.goto( c.detail_page )
+                ]);
+
+                // 業種のラベルを取得
+                try {
+                    c.sector = await page.$eval('div#industry > a', (element => {
+                        return element.innerHTML;
+                    }));
+                } catch ( e ) {
+                    c.sector = '';
+                }
+
+                //PERのラベルを取得
+                try {
+                    c.per = await page.$eval('#referenc > div > ul > li:nth-child(5) > dl > dd > a > span._1fofaCjs._2aohzPlv._1DMRub9m > span > span._3rXWJKZF._11kV6f2G', (element => {
+                        return element.innerHTML;
+                    }));
+                } catch ( e ) {
+                    c.per = '';
+                }
+
+                //PBRのラベルを取得
+                try {
+                    c.pbr = await page.$eval('#referenc > div > ul > li:nth-child(6) > dl > dd > a > span._1fofaCjs._2aohzPlv._1DMRub9m > span > span._3rXWJKZF._11kV6f2G', (element => {
+                        return element.innerHTML;
+                    }));
+                } catch ( e ) {
+                    c.pbr = '';
+                }
+
+                //EPSのラベルを取得
+                try {
+                    c.eps = await page.$eval('#referenc > div > ul > li:nth-child(7) > dl > dd > span._1fofaCjs._2aohzPlv._1DMRub9m > span > span._3rXWJKZF._11kV6f2G', (element => {
+                        return element.innerHTML;
+                    }));
+                } catch ( e ) {
+                    c.eps = '';
+                }
+
+                //BPSのラベルを取得
+                try {
+                    c.bps = await page.$eval('#referenc > div > ul > li:nth-child(8) > dl > dd > span._1fofaCjs._2aohzPlv._1DMRub9m > span > span._3rXWJKZF._11kV6f2G', (element => {
+                        return element.innerHTML;
+                    }));
+                } catch ( e ) {
+                    c.bps = '';
+                }
+
+                //単元株数のラベルを取得
+                try {
+                    c.num = await page.$eval('#referenc > div > ul > li:nth-child(10) > dl > dd > span > span > span._3rXWJKZF._11kV6f2G', (element => {
+                        return element.innerHTML;
+                    }));
+                } catch ( e ) {
+                    c.num = '';
+                }
+
+                // 取得したら前のページに戻る
+                await Promise.all([
+                    page.waitForNavigation({'waitUntil': 'networkidle2','timeout' : 30000 }),
+                    page.goBack()
+                ]);
+                console.log(c); 
             }
 
-            // 次が取得
             const next_btn = await page.$("ul.ymuiPagingBottom > a:last-child");
             if( next_btn ) {
                 // 次のページがある場合は画面遷移
                 // 表示されるまで待つ
                 await Promise.all([
-                    page.waitForNavigation({ 'waitUntil' : 'domcontentloaded', 'timeout' : 30000 }),
+                    page.waitForNavigation({ 'waitUntil' : 'networkidle0', 'timeout' : 30000 }),
                     next_btn.click()
                 ]);
 
@@ -110,7 +157,6 @@ class Company {
                 break;
             }
         }
-    
     } catch( e ) {
         console.error( e );
     } finally {
@@ -119,9 +165,5 @@ class Company {
             await browser.close();
             browser = undefined;
         }
-
     }
-
-})();
-
-
+})( );
